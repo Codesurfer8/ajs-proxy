7aad7c65d51816e405e637cea6f718ed7c3a8e9a
Sergei Korchagin <gamestationbase@gmail.com>
2023-02-12 18:23:46 +0300

    finishing adjustment of work

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -4,6 +4,28 @@ test('testing the sorting function', () => {
   const data = {
     name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
   };
+
+  const expected = [
+    { key: 'name', value: 'мечник' },
+    { key: 'level', value: 2 },
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+  ];
+
+  expect(orderByProps(data, ['name', 'level'])).toEqual(expected);
+});
+
+test('testing the sorting function', () => {
+  const data2 = {
+    id: 100,
+  };
+
+  const data = {
+    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+  };
+
+  Object.setPrototypeOf(data, data2);
   const expected = [
     { key: 'name', value: 'мечник' },
     { key: 'level', value: 2 },
